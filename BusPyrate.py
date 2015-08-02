cc66026e789ca3c04cd114bf287b7c3bd54d1402
Michal Ludvig <mludvig@logix.net.nz>
2015-08-02 16:14:24 +1200

    BusPirate detection and reset

---
+++ BusPyrate.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+# Bus Pirate Python library
+# By Michal Ludvig <mludvig@logix.net.nz> (c) 2015
+# License GPLv3
+
+import re
+import sys
+import serial
+
+class BusPyrateError(Exception):
+    #def __init__(self, message):
+    #    self.message = message
+
+    def __str__(self):
+        return "%s" % self.args
+
+class BusPyrate(object):
+    bp_version = None
+    bp_firmware = None
+    bp_bootloader = None
+
+    def __init__(self, device, speed = 115200, binmode = True):
+        self._ser = serial.Serial(port = device, baudrate = speed)
+
+        # Test if BusPirate talks to us
+        self._ser.setTimeout(0.5)   # Give it more time to respond
+        tries = 5                   # and try few CR+NLs before it syncs
+        bp_ok = False
+        while tries:
+            self._ser.write(b"\r\n")
+            buf = self._ser.readall().decode('ascii')
+            if buf.endswith(">"):
+                bp_ok = True
+                break
+            tries -= 1
+
+        if not bp_ok:
+            raise BusPyrateError("BusPirate is in unknown state. Reset it and try again.")
+
+        # Now it should be quicker
+        self._ser.setTimeout(0.2)
+
+        # Reset to a known state
+        self._ser.write(b"#\n")
+        while True:
+            buf = self._ser.readline().decode('ascii').strip()
+
+            # Bus Pirate v3
+            m = re.match("Bus Pirate (v.*)", buf)
+            if m:
+                self.bp_version = m.group(1)
+
+            # Firmware v4.2 Bootloader v4.1
+            m = re.match("Firmware v(\d+)\.(\d+) Bootloader v(\d+)\.(\d+)", buf)
+            if m:
+                self.bp_firmware = 100 * int(m.group(1)) + int(m.group(2))
+                self.bp_bootloader = 100 * int(m.group(3)) + int(m.group(4))
+
+            if buf.endswith("HiZ>"):
+                break
+
+        print("Detected BusPirate %s (firmware %d.%d)" % (self.bp_version, self.bp_firmware / 100, self.bp_firmware % 100))
+        self._ser.close()
+
+if __name__ == "__main__":
+    try:
+        bp = BusPyrate(device = "/dev/ttyUSB0")
+    except Exception as e:
+        sys.stderr.write(str(e) + "\n")
+        sys.exit(1)
