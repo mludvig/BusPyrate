91e1295061e3ead78a0d44ada32156cd87bfbe16
Michal Ludvig <mludvig@logix.net.nz>
2015-08-02 16:49:50 +1200

    Enter BinMode and Reset from BinMode

--- BusPyrate.py
+++ BusPyrate.py
@@ -19,6 +19,7 @@ class BusPyrate(object):
     bp_version = None
     bp_firmware = None
     bp_bootloader = None
+    bp_binmode = None
 
     def __str__(self):
         return "BusPirate %s (Firmware %d.%d)" % (self.bp_version, self.bp_firmware / 100, self.bp_firmware % 100)
@@ -31,24 +32,46 @@ class BusPyrate(object):
         tries = 5                   # and try few CR+NLs before it syncs
         bp_ok = False
         while tries:
+            # Test BinMode
+            self._ser.sendBreak()
+            buf = self._ser.readall().decode('ascii')
+            m = re.search("BBIO(\d+)", buf)
+            if m:
+                self.bp_binmode = int(m.group(1))
+                break
+
+            # Test TextMode
             self._ser.write(b"\r\n")
+            self._ser.flush()
             buf = self._ser.readall().decode('ascii')
             if buf.endswith(">"):
-                bp_ok = True
+                self.bp_binmode = 0     # bp_binmode==0 -> text mode :)
                 break
+
             tries -= 1
 
-        if not bp_ok:
+        if self.bp_binmode is None:
             raise BusPyrateError("BusPirate is in unknown state. Reset it and try again.")
 
+        self.reset()
+
+    def reset(self):
         # Reset to a known state
-        self.reset_from_textmode()
+        if self.bp_binmode:
+            self._reset_from_binmode()
+        else:
+            self._reset_from_textmode()
 
-    def reset_from_textmode(self):
+    def _reset_from_binmode(self):
+        self._ser.write(b"\x00\x0F")
+        self._ser.flush()
+        self._reset_parse()
+
+    def _reset_from_textmode(self):
         self._ser.write(b"#\n")
-        self.reset_parse()
+        self._reset_parse()
 
-    def reset_parse(self):
+    def _reset_parse(self):
         self._ser.setTimeout(0.2)
         while True:
             buf = self._ser.readline().decode('ascii').strip()
@@ -67,10 +90,26 @@ class BusPyrate(object):
             if buf.endswith("HiZ>"):
                 break
 
+    def enter_binmode(self):
+        tries = 20
+        while tries:
+            self._ser.write(b"\x00\x00")
+            self._ser.flush()
+            buf = self._ser.readall().decode('ascii')
+            m = re.search("BBIO(\d+)", buf)
+            if m:
+                self.bp_binmode = int(m.group(1))
+                return True
+            tries -= 1
+        raise BusPyrateError("Unable to enter binary mode.")
+
 if __name__ == "__main__":
     try:
         bp = BusPyrate(device = "/dev/ttyUSB0")
         print(bp)
+        bp.enter_binmode()
+        print("Binary mode version %d" % bp.bp_binmode)
+        bp.reset()
     except Exception as e:
         sys.stderr.write(str(e) + "\n")
         sys.exit(1)
